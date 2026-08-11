1b91f8f25883c46626613b67b575a7862c72d40e
YanVelasco <yancarlosduartevelasco@gmail.com>
2026-08-11 15:35:09 -0300

    add response time header to bank service routes in GatewayserverApplication

--- gatewayserver/src/main/java/com/bank/gatewayserver/GatewayserverApplication.java
+++ gatewayserver/src/main/java/com/bank/gatewayserver/GatewayserverApplication.java
@@ -6,32 +6,36 @@ import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
 
+import java.time.LocalDateTime;
+
 @SpringBootApplication
 public class GatewayserverApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(GatewayserverApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayserverApplication.class, args);
+    }
 
-	@Bean
-	public RouteLocator myRoutes(RouteLocatorBuilder routeLocatorBuilder) {
-		return routeLocatorBuilder
-				.routes()
-				.route(p -> p
-						.path("/bank/accounts/**")
-						.filters(f -> f.rewritePath("/bank/accounts/(?<segment>.*)", "/${segment}"))
-						.uri("lb://ACCOUNTS")
-				)
-				.route(p -> p
-						.path("/bank/cards/**")
-						.filters(f -> f.rewritePath("/bank/cards/(?<segment>.*)", "/${segment}"))
-						.uri("lb://CARDS")
-				)
-				.route(p -> p
-						.path("/bank/loans/**")
-						.filters(f -> f.rewritePath("/bank/loans/(?<segment>.*)", "/${segment}"))
-						.uri("lb://LOANS")
-				).build();
-	}
+    @Bean
+    public RouteLocator myRoutes(RouteLocatorBuilder routeLocatorBuilder) {
+        return routeLocatorBuilder
+                .routes()
+                .route(p -> p
+                        .path("/bank/accounts/**")
+                        .filters(f -> f.rewritePath("/bank/accounts/(?<segment>.*)", "/${segment}").addResponseHeader("X-Response-Time", LocalDateTime.now().toString()))
+                        .uri("lb://ACCOUNTS")
+                )
+                .route(p -> p
+                        .path("/bank/cards/**")
+                        .filters(f -> f.rewritePath("/bank/cards/(?<segment>.*)", "/${segment}")
+                                .addResponseHeader("X-Response-Time", LocalDateTime.now().toString()))
+                        .uri("lb://CARDS")
+                )
+                .route(p -> p
+                        .path("/bank/loans/**")
+                        .filters(f -> f.rewritePath("/bank/loans/(?<segment>.*)", "/${segment}")
+                                .addResponseHeader("X-Response-Time", LocalDateTime.now().toString()))
+                        .uri("lb://LOANS")
+                ).build();
+    }
 
 }
